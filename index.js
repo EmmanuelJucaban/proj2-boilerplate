672835069c818a41952fcdbd28551edb98ce0a2d
emmanueljucaban <emmanueljucaban@gmail.com>
2020-10-31 12:14:34 -0700

    F - Prep boilerplate for heroku production

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ require('./services/passport');
 
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log('Server started listening on PORT http://localhost:3001');
 });
---
+++ readme.md
@@ -0,0 +1,4 @@
+You need to create a .env file that has a JWT_SECRET.
+The content of the file should look like the code below.
+
+JWT_SECRET=asdyuofgayudagdyuadsgar837rgsdada
