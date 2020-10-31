2697e8ac64e52ddb5b3e00cdeacbe56dba5fe0d6
emmanueljucaban <emmanueljucaban@gmail.com>
2020-10-31 09:35:11 -0700

    F - Fixed file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import NavTabs from "./components/NavTabs";
-import Navbar from './components/Navbar';
-
-
-// You can also just do the code below
-// import {
-//   UserContainer,
-//   SignUp,
-// } from './pages';
-
-import {
-  UserContainer
-} from './pages/User';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './pages/common/components/Navbar';
 
 import {
   WrappedSignUp,
   WrappedSignIn,
 } from './pages/Viewer';
 
-// import Contact from "./components/pages/Contact";
+
 
 function App() {
   return (
@@ -27,7 +15,9 @@ function App() {
       <Navbar/>
       <Route path='/signup' component={WrappedSignUp}/>
       <Route path='/signin' component={WrappedSignIn}/>
-      <Route exact path="/" component={About}/>
+      <Route exact path="/">
+        <h1>Welcome to the about</h1>
+      </Route>
     </Router>
   );
 }
