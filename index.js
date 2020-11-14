4c6f1c869dc400a0680c4e01a23d5a7ae0c1bab2
emmanueljucaban <emmanueljucaban@gmail.com>
2020-11-14 12:13:50 -0800

    Manny suggestion 6

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
