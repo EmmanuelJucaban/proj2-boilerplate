c1082726a8d142b4a988a6ef50dd9e0e93eb6284
emmanueljucaban <emmanueljucaban@gmail.com>
2020-11-14 12:17:18 -0800

    Manny suggestion 6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv')
   .config();
 const express = require('express');
 const routes = require('./routes');
-
+const path = require('path');
 require('./services/passport');
 
 const app = express();
